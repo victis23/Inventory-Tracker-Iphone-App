e099a878ed0480457105d01c7587dcb98702fe8c
Scott Leonard <wow@duhmarket.com>
2019-10-06 20:31:49 -0400

    Added Classes For Controllers

---
+++ IphoneInventoryTracker/InventoryDetailCell_CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  InventoryDetailCell_CollectionViewCell.swift
+//  IphoneInventoryTracker
+//
+//  Created by Scott Leonard on 10/6/19.
+//  Copyright © 2019 Scott Leonard. All rights reserved.
+//
+
+import UIKit
+
+class InventoryDetailCell_CollectionViewCell: UICollectionViewCell {
+    
+}
