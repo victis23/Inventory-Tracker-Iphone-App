49ec86398b7562c2ace21a9edfae59f42ff67305
Scott Leonard <wow@duhmarket.com>
2019-10-14 10:36:56 -0400

    added submit button and Corresponding IBOutlet Removed editdidchange: method

--- IphoneInventoryTracker/Controllers/NewOrderController/NewOrder_TableViewController.swift
+++ IphoneInventoryTracker/Controllers/NewOrderController/NewOrder_TableViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class NewOrder_TableViewController: UITableViewController {
+class NewOrder_TableViewController: UITableViewController, UITextFieldDelegate {
 	
 	@IBOutlet weak var currentAmount: UILabel!
 	@IBOutlet weak var orderAmount: UITableViewCell!
 	@IBOutlet weak var needAmountLabel: UITextField!
+	@IBOutlet weak var submitButton :UIButton!
+
 	var stockObject : Stock!
 	
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		setupSubmitButtonAsthetics()
 		amountLabelText(stockObject)
     }
+	
+	func setupSubmitButtonAsthetics(){
+		submitButton.layer.cornerRadius = 10
+	}
 	
 	func setAmountLabel(_ model: Stock){
 		stockObject = model
@@ -32,16 +39,8 @@ class NewOrder_TableViewController: UITableViewController {
 	
 	//MARK: IBActions
 	
-	@IBAction func updatedAmount(_ sender: UITextField) {
-		
-		guard let neededAmount = needAmountLabel.text, let intNeedAmount = Int(neededAmount), let amount = stockObject.amount else {return}
-		
-		let newAmount = amount - intNeedAmount
-		let amountText :String = "\(newAmount)"
-		currentAmount.text = amountText
-		print(amountText)
-	}
-
+	
+	
 }
 
 
