ae859c65cd786c4efa26b55c735fd214b41c898a
Scott Leonard <wow@duhmarket.com>
2019-11-09 08:13:42 -0500

    Added Paper Calculator View Controller And story board

---
+++ IphoneInventoryTracker/PaperCalculatorModules/Controller/PaperCalculatorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PaperCalculatorViewController.swift
+//  IphoneInventoryTracker
+//
+//  Created by Scott Leonard on 11/9/19.
+//  Copyright © 2019 Scott Leonard. All rights reserved.
+//
+
+import UIKit
+
+class PaperCalculatorViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
