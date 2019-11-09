1d523b4b211f0a3b45d05343669947fb101f591b
Scott Leonard <wow@duhmarket.com>
2019-11-09 09:05:31 -0500

    Added UIelements to PaperCalculator Storyboard Added function to control aesthetics on controller for PaperCalculator

--- IphoneInventoryTracker/PaperCalculatorModules/Controller/PaperCalculatorViewController.swift
+++ IphoneInventoryTracker/PaperCalculatorModules/Controller/PaperCalculatorViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class PaperCalculatorViewController: UITableViewController {
-
+	
+	//MARK: IBOUTLETS
+	
+	@IBOutlet weak var mainStack: UIStackView!
+	@IBOutlet weak var parentSheetStack: UIStackView!
+	@IBOutlet weak var childSheetStack: UIStackView!
+	@IBOutlet weak var submitButton: UIButton!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
+		setupAesthetic()
     }
-    
-
+	
+	func setupAesthetic(){
+		tableView.keyboardDismissMode = .interactive
+		submitButton.layer.cornerRadius = 10
+		submitButton.setTitle("Submit", for: .normal)
+	}
+	
+	@IBAction func tappedSubmitButton(_ sender: Any) {
+		parentSheetStack.isHidden = true
+		childSheetStack.isHidden = true
+	}
 }
