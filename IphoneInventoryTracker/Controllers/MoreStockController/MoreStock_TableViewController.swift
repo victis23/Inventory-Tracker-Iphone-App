5bd0bc063a173a8360b7e8806503b827269da2dc
Scott Leonard <wow@duhmarket.com>
2019-11-10 07:14:42 -0500

    Fixed calculation bug within the MoreStock controller class. Added documentation.

--- IphoneInventoryTracker/Controllers/MoreStockController/MoreStock_TableViewController.swift
+++ IphoneInventoryTracker/Controllers/MoreStockController/MoreStock_TableViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class MoreStock_TableViewController: UITableViewController {
 	
+	//MARK: Keys
+	// Keys for Segues
 	private struct SegueKeys {
 		static var home = "home"
 	}
 	
+	// MARK: IBOutlets
+	
 	@IBOutlet weak var amountLabel: UILabel!
 	@IBOutlet weak var submitButton: UIButton!
 	@IBOutlet weak var newAmount: UITextField!
+	
+	//MARK: Class Properties
+	
 	var incomingStock : Stock!
 	var indexPath : IndexPath?
+	
+	//MARK: State
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,16 +37,26 @@ class MoreStock_TableViewController: UITableViewController {
 		tableView.keyboardDismissMode = .interactive
     }
 	
+	/// Assigns incoming instance of `Stock` to local property.
+	/// - Note: This method is called duing segue to this view.
+	/// - Parameter incomingModel: Recieved Stock Object from `InventoryTracker_CollectionViewController.swift`.
 	func setModelForController(_ incomingModel : Stock){
 		incomingStock = incomingModel
 	}
+	
+	/// Updates  local variable with the value of amount property from incoming `Stock` object.
+	/// Assigns this value to `text` property of amountLabel.
 	func setAmountLabel(){
 		guard let amount = incomingStock?.amount else {return}
 		amountLabel.text = String(amount)
 	}
+	
+	/// Sets title that will be displayed in navigation controller.
 	func setControllerTitle(){
 		self.title = "Add Stock For \(incomingStock?.name ?? "Inventory List")"
 	}
+	
+	/// Modifies visual look for the view upon `viewDidLoad()`.
 	func setButtonAesthetics(){
 		submitButton.layer.cornerRadius = 10
 		submitButton.setTitle("Update", for: .normal)
@@ -58,20 +77,26 @@ class MoreStock_TableViewController: UITableViewController {
 	//MARK: IBActions
 	
 	@IBAction func submitButtonTapped(_ sender: Any) {
+		// Unwraps values and assigns the new amount added by user to the current total.
+		// newAmount : The new amount added by user. —> amount2 is this value as a string.
+		// oldAmount : This is the current amount of inventory.
 		guard let amount2 = newAmount.text, let newAmount = Int(amount2), let oldAmount = incomingStock.amount else {return}
 		incomingStock?.amount = newAmount + oldAmount
 		view.endEditing(true)
-		
 		guard let amount = incomingStock?.amount, let spentTotal = incomingStock.spent else {return}
 		amountLabel.text = String(amount)
+		// Uses new values to update percent of inventory in comparison to recommended amount.
 		incomingStock?.setPercentageAmount()
 		// Updates the total amount spent by adding to the original amount.
-		incomingStock.spent = updateSpentAmount(with: amount) + spentTotal
+		// The Amount used needs to be the amount prior to it being added to the total!
+		incomingStock.spent = updateSpentAmount(with: newAmount) + spentTotal
 		Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { (timer) in
 			self.performSegue(withIdentifier: SegueKeys.home, sender: self.incomingStock)
 		}
 	}
 	//MARK: TableView Delegate
+	
+	// Does nothing other than deselecting the current cell and hiding keyboard. The purpose of this method is primarily just to hide the keyboard.
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		view.endEditing(true)
 		tableView.deselectRow(at: indexPath, animated: true)
@@ -79,20 +104,26 @@ class MoreStock_TableViewController: UITableViewController {
 	
 	//MARK: Navigation
 	
+	
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		if segue.identifier == SegueKeys.home {
 			guard let homeController = segue.destination as? InventoryTracker_CollectionViewController else {return}
-			
+			// Gets indexPath using model identifier.
 			indexPath = homeController.dataSource.indexPath(for: incomingStock)
-			
+			// Creates a empty collection that will be holding the collect of stock that we will be appending to.
 			var localStocks :[Stock] = []
+			// Unwrapping collection of Stock from InventoryTracker...
 			guard let externalStock = homeController.stock else {return}
-			
+			// Assigns this collect to local instance.
 			localStocks = externalStock
+			// If statement is just a checker - Not Required.
+			// Removes the old value for the specified IndexPath, then assigns our local value.
+			// If the old value is not removed you will be recieving a non-unique identifier error.
 			if indexPath?.item != nil {
 				localStocks.remove(at: indexPath!.item)
 				localStocks.insert(incomingStock, at: indexPath!.item)
 			}
+			// Updates the collection of stock on InventoryTracker... with the local value.
 			homeController.stock = localStocks
 		}
 	}
--- IphoneInventoryTracker/PaperCalculatorModules/Controller/CostOfSpentGoodsTableViewController.swift
+++ IphoneInventoryTracker/PaperCalculatorModules/Controller/CostOfSpentGoodsTableViewController.swift
@@ -10,32 +10,45 @@ import UIKit
 
 class CostOfSpentGoodsTableViewController: UITableViewController {
 	
+	//MARK: Local Data types
+	
+	//Sections used for DataSource.
 	enum Section {
 		case main
 	}
 	
+	//KEYS
 	struct Keys {
 		static var uniqueCellIdentifer = "cell"
 	}
-
-
+	
+	//MARK: Class Properties
 	
 	var dataSource : UITableViewDiffableDataSource<Section,Stock>? = nil
 	var stock : [Stock]? = []
 	
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	//MARK: State
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
 		retrieveInventoryListFromFile()
 		setupDataSource()
-    }
+	}
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		createSnapShot()
 	}
 	
+	//MARK: Methods
+	
+	/// Decodes list of stocks from disk and assigns to local collection.
+	/// - Note: Object method returns a generic  <T: Hashable>.
 	func retrieveInventoryListFromFile(){
+		// Method returns a generic that needs to be specified.
 		stock = Stock.decode() as [Stock]?
 	}
+	
+	//MARK: - TableView DataSource & Delegate Methods.
 	
 	func setupDataSource(){
 		dataSource = UITableViewDiffableDataSource<Section,Stock>(tableView: tableView, cellProvider: { (tableView, indexPath, stock) -> UITableViewCell? in
@@ -45,6 +58,7 @@ class CostOfSpentGoodsTableViewController: UITableViewController {
 			cell.nameLabel.text = stock.name
 			guard let spent = stock.spent else {fatalError()}
 			
+			// Adds two decimal points to value. $0.00
 			cell.costLabel.text = "$\(String(format: "%.2f", spent))"
 			return cell
 		})
