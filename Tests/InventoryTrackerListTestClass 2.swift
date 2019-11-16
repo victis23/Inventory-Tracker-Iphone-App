c47e23a2bc6bc714eeded11032bdd6da87c5e44d
Michael Wells <wow@duhmarket.com>
2019-11-16 16:00:31 -0500

    Removed

---
+++ Tests/InventoryTrackerListTestClass 2.swift
@@ -0,0 +1,25 @@
+//
+//  InventoryTrackerListTestClass.swift
+//  IphoneInventoryTracker
+//
+//  Created by Scott Leonard on 11/13/19.
+//  Copyright © 2019 Scott Leonard. All rights reserved.
+//
+
+import XCTest
+@testable import IphoneInventoryTracker
+
+class InventoryTrackerListTestClass: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testingRemoveDecimals(){
+		
+    }
+}
