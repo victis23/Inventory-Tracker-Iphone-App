d3211dce9dc536ebaab44f088be0fe75d55325f6
Michael Wells <wow@duhmarket.com>
2020-01-20 21:05:25 -0500

    Add Test

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -10,19 +10,49 @@ import XCTest
 @testable import Inventory_Tracker
 
 class Tests: XCTestCase {
-	let vc = InventoryTracker_CollectionViewController()
+	
+	var vc : InventoryTracker_CollectionViewController!
+	var _10Envelopes : Stock!
+	var _postCardCounts : Stock!
 	
 	override func setUp() {
-		// Put setup code here. This method is called before the invocation of each test method in the class.
+		vc = InventoryTracker_CollectionViewController()
+		vc.costDelegate = nil
+		_postCardCounts = Stock()
 	}
 	
 	override func tearDown() {
-		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		
 	}
 	
-	func testListIsEmpty(){
+	func testCostDelegateNotNil(){
+		vc.costDelegate = CostOfSpentGoodsTableViewController()
+		XCTAssertNotNil(vc.costDelegate)
+	}
+	
+	func testCostDelegateIsNil(){
 		XCTAssertNil(vc.costDelegate)
 	}
 	
+	// Tests whether calculations are being done correctly for envelopes.
+	func testPerformCalculationsForEnvelopes() throws {
+		_10Envelopes = Stock("Test", ._10Envelope, ._20Bond, 1000, 100, nil)
+		
+		 try _10Envelopes.performCalculations(amountOfPiecesNeededForOrder: 500, shortEdge: 4.125, longEdge: 9.5)
+		
+		XCTAssertTrue(_10Envelopes.amount == 500)
+	}
+	
+	// Tests whether calculations are being done correctly for normal stock.
+	func testNormalCalculations() throws {
+		_postCardCounts.amount = 100
+		_postCardCounts.parentSheetSize = .oversized
+		_postCardCounts.name = "Heavy Post Cards"
+		_postCardCounts.weight = ._12PtC2S
+		
+		try _postCardCounts.performCalculations(amountOfPiecesNeededForOrder: 100, shortEdge: 4.25, longEdge: 6.25)
+		
+		XCTAssertTrue(_postCardCounts.amount == 75)
+	}
 	
 }
