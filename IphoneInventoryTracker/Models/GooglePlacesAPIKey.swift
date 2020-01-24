2f28d99540a56b5ba2261eb3671d41b799f891f8
Michael Wells <wow@duhmarket.com>
2020-01-24 16:10:14 -0500

    Abstraction of api key

---
+++ IphoneInventoryTracker/Models/GooglePlacesAPIKey.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseKey.swift
+//  IphoneInventoryTracker
+//
+//  Created by Scott Leonard on 1/24/20.
+//  Copyright © 2020 Scott Leonard. All rights reserved.
+//
+
+import Foundation
+
+enum GooglePlacesAPIKey : String{
+	case apiKey = "AIzaSyBl6VYW1Vh32RDdZ8DUtnmIAvsCw1Icunw"
+}
