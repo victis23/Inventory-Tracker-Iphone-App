929278c9fc1e3798749f5b34736ebe0d1f9dc0e5
Michael Wells <wow@duhmarket.com>
2020-01-24 17:24:43 -0500

    update comment

--- IphoneInventoryTracker/AppDelegate.swift
+++ IphoneInventoryTracker/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		
-		// Key is now hidden from github
+		// Key is now hidden from github and in private file.
 		GMSServices.provideAPIKey(GooglePlacesAPIKey.apiKey.rawValue)
 		
 		GMSPlacesClient.provideAPIKey(GooglePlacesAPIKey.apiKey.rawValue)
